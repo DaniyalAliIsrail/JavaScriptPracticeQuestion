b31ce199c10e7ceb4c7964c1bbbc27f792c2a1de
daniyal ali <daniyalali12568@gmail.com>
2024-04-01 03:38:11 +0500

    call-apply-bind

--- call-apply-bind/call-and-this.js
+++ call-apply-bind/call-and-this.js
@@ -23,10 +23,16 @@ console.log(userName1.fullName.call(userName2))
 
 
 // method:02
+//function no patameter
 let userNameFun = function(){
     console.log(this.firstName + " " + this.lastName)
 }
 
+//function with parameter
+let userNameFunwithstate = function(city,country){
+    console.log(this.firstName + " " + this.lastName + " "+"state" + " " + city + " " + country)
+}
+//userName 3
 let userName3 = {
     firstName:"daniyal",
     lastName:"ali",
@@ -37,14 +43,21 @@ let userName3 = {
 
 userNameFun.call(userName3)
 
+//userName 4
 let userName4 = {
     firstName:"soft",
     lastName:"ware",
     class:"Eight",
     Age:20
 }
+//passed with string , Array or Obejct 
+userNameFunwithstate.call(userName4,"karachi","pakistan");
 
-userNameFun.call(userName4)
+// apply k help say ham multiple argument array may pass karsaktay hay
+userNameFunwithstate.apply(userName4,["karachi","pakistan","us"]);
+
+// bind function same as call par function but bind function ko direct call nhy kara saktay bind may function ka copy banatay hay or var may store kara letay hay or condition kay mutabiq call kara letay hay.....
+// ++++++++++++++++++++++ Chai or  Code || Call and this ++++++++++++++++
 
 
 
