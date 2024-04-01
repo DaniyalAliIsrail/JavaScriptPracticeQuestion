56acc5b0d47eb9fd26e66e3b3c6476923085e5b5
daniyal ali <daniyalali12568@gmail.com>
2024-04-01 05:41:10 +0500

    map-pollyfills

--- polyfills-Arrays-interview/map-polyfills.js
+++ polyfills-Arrays-interview/map-polyfills.js
@@ -1,17 +1,34 @@
 //write a polyfill for the map function in javascript;
-let arr1 = [1,2,3,4,5]
-Array.prototype.myMap1 = function(cb){
+// myMap function ko js array kay sath call karo gey this use array ko consider karay ag or usy array par sara operation karya ga
+
+// let arr1 = [1,2,3,4,5]
+// Array.prototype.myMap1 = function(cb){
+//     let newArray = [];
+//     for(i=0; i<this.length; i++){
+//         newArray.push(cb(this[i],i,arr1))
+//     }
+//     return newArray
+// }
+// const data = arr1.myMap1((item,index,orginalArray)=>{
+//     console.log(item,index,orginalArray);
+//     return item * 2
+// })
+// console.log(data);
+
+Array.prototype.myMap1 = function(cb) {
     let newArray = [];
-    for(i=0; i<this.length; i++){
-        newArray.push(cb(this[i],i,arr1))
+    for(let i = 0; i < this.length; i++) {
+        newArray.push(cb(this[i]),i,this[i]);
     }
-    return newArray
+    return newArray;
 }
 
-const data = arr1.myMap1((item,index,orginalArray)=>{
-    console.log(item,index,orginalArray);
-    return item * 2
-})
-console.log(data);
+let arr = [1, 2, 3, 4, 5];
 
+let square = function(x) {
+    return x * x;
+}
+
+const myFun = arr.myMap1(square);
+console.log(myFun);
 
