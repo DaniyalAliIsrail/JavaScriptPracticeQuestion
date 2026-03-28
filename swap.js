7949cb5f4abafa5c9187ac5e1730642d1f734faf
Daniyal Ali <daniyalali12568@gmail.com>
2026-03-28 14:39:32 +0500

    Swap variables using arithmetic (no temp)
    Replace the temp-variable based swap in swap.js with an arithmetic swap (a = a + b; b = a - b; a = a - b). The original temp-based code is preserved as commented lines. Initial values were adjusted (b changed from 20 to 5) and console logs remain to show the results. This demonstrates swapping values without allocating a temporary variable.

--- swap.js
+++ swap.js
@@ -1,16 +1,27 @@
-let a = 10;
-let b = 20;
-console.log("The value of a is",a);
-console.log("The value of b is",b);
-let temp=a
-a=b;
-b=temp;
+// let a = 10;
+// let b = 20;
+// console.log("The value of a is",a);
+// console.log("The value of b is",b);
+// let temp=a
+// a=b;
+// b=temp;
+// console.log("After swap the two varibale");
+// console.log("the value of a is ",a);
+// console.log("The value of b is",b);
+
+
+
+
+let a = 10 
+let b = 5
+
+a = a + b; //15
+b = a - b // 10
+a = a -b // 5
+
 console.log("After swap the two varibale");
 console.log("the value of a is ",a);
 console.log("The value of b is",b);
-
-
-
 
 
 
